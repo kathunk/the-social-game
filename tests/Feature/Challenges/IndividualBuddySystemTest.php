821f2e3170e8e3139ee9d00a32bdc414c2b97e0f
John Drexler <johnrudolphdrexler@gmail.com>
2025-05-20 07:43:35 -0400

    WIP

---
+++ tests/Feature/Challenges/IndividualBuddySystemTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use App\Challenges\Classes\IndividualBuddySystem;
+use App\Livewire\GameDashboard;
+use App\Models\Challenge;
+use Livewire\Livewire;
+use Thunk\Verbs\Facades\Verbs;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+it('runs individual buddy system', function () {
+    Verbs::commitImmediately();
+
+    $challenges = [
+        [
+            'challenge_keys' => [IndividualBuddySystem::key()],
+            'duration' => 10,
+        ],
+    ];
+
+    $this->mockGameTemplate(
+        challenges: $challenges,
+        type: 'individual',
+    );
+
+    $this->createGame();
+
+    $player_1 = $this->createPlayer();
+    $player_2 = $this->createPlayer();
+    $player_3 = $this->createPlayer();
+    $player_4 = $this->createPlayer();
+
+    $this->game->start();
+
+    $challenge = Challenge::first();
+
+    $this->actingAs($player_1->user);
+
+    Livewire::test(GameDashboard::class, ['game' => $this->game->fresh()])
+        ->set('challenge_properties.upvote_player_id', $player_2->id)
+        ->set('challenge_properties.downvote_player_id', $player_3->id)
+        ->call('callChallengeAction', 'vote');
+
+    $this->actingAs($player_2->user);
+
+    Livewire::test(GameDashboard::class, ['game' => $this->game->fresh()])
+        ->set('challenge_properties.upvote_player_id', $player_1->id)
+        ->set('challenge_properties.downvote_player_id', $player_4->id)
+        ->call('callChallengeAction', 'vote');
+
+    $this->actingAs($player_3->user);
+
+    Livewire::test(GameDashboard::class, ['game' => $this->game->fresh()])
+        ->set('challenge_properties.upvote_player_id', $player_4->id)
+        ->set('challenge_properties.downvote_player_id', $player_1->id)
+        ->call('callChallengeAction', 'vote');
+
+    $challenge->refresh();
+    $challenge->end();
+
+    // visible scores show this
+    expect($player_1->fresh()->score)->toBe(0);
+    expect($player_2->fresh()->score)->toBe(1);
+    expect($player_3->fresh()->score)->toBe(-1);
+    expect($player_4->fresh()->score)->toBe(0);
+
+    // but with hidden scores included, we see this
+    expect($player_1->fresh()->state()->score(include_hidden: true))->toBe(1);
+    expect($player_2->fresh()->state()->score(include_hidden: true))->toBe(2);
+    expect($player_3->fresh()->state()->score(include_hidden: true))->toBe(-1);
+    expect($player_4->fresh()->state()->score(include_hidden: true))->toBe(0);
+});
