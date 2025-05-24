1d22b2367af58fef18b868bdcde9d39db6981c27
Jacob Davis <jacobamdavis@gmail.com>
2025-05-24 15:01:44 -0400

    elixir & go, small next button update

--- resources/views/livewire/next-challenge-button.blade.php
+++ resources/views/livewire/next-challenge-button.blade.php
@@ -2,7 +2,7 @@
     @if($this->show)
         <flux:button
             wire:click="nextChallenge"
-            class="!bg-gradient-to-tr !from-indigo-300 !to-green-300 !w-fit"
+            class="!bg-gradient-to-tr !from-indigo-300 !to-green-300 !w-fit !text-gray-700"
             icon="forward"
             size="sm"
         >
