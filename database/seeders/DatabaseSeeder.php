b8b54098891f7e86a8773218394b2384ba11b961
John Drexler <johnrudolphdrexler@gmail.com>
2025-05-30 09:37:16 -0400

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            // Laracon2025Seeder::class,
-            // PeckingOrderSeeder::class,
-            BloodOathSeeder::class,
+            Laracon2025Seeder::class,
+            PeckingOrderSeeder::class,
+            // BloodOathSeeder::class,
         ]);
     }
 }
