dc62957208ec380672968837c44d14863de668f0
John Drexler <johnrudolphdrexler@gmail.com>
2025-06-12 18:49:57 -0400

    WIP

--- resources/views/livewire/secrets-page.blade.php
+++ resources/views/livewire/secrets-page.blade.php
@@ -1,7 +1,9 @@
 <div>
-    <flux:link :href="route('game-dashboard', $this->game)" variant="ghost">
-        Back to game
-    </flux:link>
+    <div class="mb-4">
+        <flux:link :href="route('game-dashboard', $this->game)" variant="ghost">
+            Back to game
+        </flux:link>
+    </div>
 
     <x-game-components.form :form="$this->modifier->handler()->frontendComponentForDedicatedPage($this->player)" type="modifier" class_key="{{ $this->modifier->class_key }}" />
 </div>
