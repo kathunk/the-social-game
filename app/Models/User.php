ee3ebbac7fd20a5921a995e9796e8a1d55d5ee84
John Drexler <johnrudolphdrexler@gmail.com>
2025-06-13 08:08:16 -0400

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -209,4 +209,9 @@ class User extends Authenticatable
     {
         return 'https://www.gravatar.com/avatar/'.md5(strtolower(trim($this->email)));
     }
+
+    public function getHasActiveGameAttribute(): bool
+    {
+        return $this->games->where('status', 'active')->isNotEmpty();
+    }
 }
--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -63,13 +63,11 @@
                         </div>
                     </flux:menu.radio.group>
 
-                    @if (!$user?->currentPlayer)
-                        <flux:menu.separator />
+                    <flux:menu.separator />
 
-                        <flux:menu.radio.group>
-                            <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                        </flux:menu.radio.group>
-                    @endif
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                    </flux:menu.radio.group>
 
                     <flux:menu.separator />
 
