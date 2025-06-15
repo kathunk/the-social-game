e940114e50d19e967ebe3613a86947c3998ba755
John Drexler <johnrudolphdrexler@gmail.com>
2025-06-15 06:52:00 -0400

    WIP

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -10,4 +10,11 @@
     @if ($this->games->filter(fn ($game) => $game->status === 'ended')->count() > 0)
         <x-menus.game-card :games="$this->games->filter(fn ($game) => $game->status === 'ended')" status="ended" />
     @endif
+
+    @if ($this->games->count() === 0)
+        <flux:card class="flex flex-col items-center justify-center">
+            <flux:heading>No games found</flux:heading>
+            <flux:button variant="primary" :href="route('create-game')" class="mt-4">New Game</flux:button>
+        </flux:card>
+    @endif
 </div>
