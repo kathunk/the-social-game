9ab9ef7a9b4f5ee0d9e34a4736050cc02dd8610e
John Drexler <johnrudolphdrexler@gmail.com>
2025-06-24 12:56:55 -0400

    WIP

--- resources/views/components/game-components/countdown-timer.blade.php
+++ resources/views/components/game-components/countdown-timer.blade.php
@@ -1,28 +1,34 @@
 @props(['ends_at'])
 
-<div
-    wire:ignore
-    x-data="{
-        endTime: new Date('{{ $ends_at }}'),
-        now: new Date(),
-        timeLeft: '',
-        interval: null,
-        tick() {
-            let seconds = Math.floor((this.endTime - new Date()) / 1000);
-            if (seconds <= 0) {
-                this.timeLeft = 'ending...';
-                return;
-            }
+@if ($ends_at < now()->addMinutes(60))
+    <div>
+        ends {{ $ends_at->diffForHumans() }}
+    </div>
+@else
+    <div
+        wire:ignore
+        x-data="{
+            endTime: new Date('{{ $ends_at }}'),
+            now: new Date(),
+            timeLeft: '',
+            interval: null,
+            tick() {
+                let seconds = Math.floor((this.endTime - new Date()) / 1000);
+                if (seconds <= 0) {
+                    this.timeLeft = 'ending...';
+                    return;
+                }
 
-            const m = Math.floor(seconds / 60).toString().padStart(2, '0');
-            const s = (seconds % 60).toString().padStart(2, '0');
-            this.timeLeft = `ends in ${m}:${s}`;
-        }
-    }"
-    x-init="
-        tick();
-        interval = setInterval(() => tick(), 1000);
-    "
-    @unload="clearInterval(interval)"
-    x-text="timeLeft"
-></div>
+                const m = Math.floor(seconds / 60).toString().padStart(2, '0');
+                const s = (seconds % 60).toString().padStart(2, '0');
+                this.timeLeft = `ends in ${m}:${s}`;
+            }
+        }"
+        x-init="
+            tick();
+            interval = setInterval(() => tick(), 1000);
+        "
+        @unload="clearInterval(interval)"
+        x-text="timeLeft"
+    ></div>
+@endif
