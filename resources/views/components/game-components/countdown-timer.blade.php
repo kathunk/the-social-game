76a9eb217e9aa6d0a4b0b95ed9e98a4a50af6af8
John Drexler <johnrudolphdrexler@gmail.com>
2025-06-28 08:32:40 -0400

    WIP

--- resources/views/components/game-components/countdown-timer.blade.php
+++ resources/views/components/game-components/countdown-timer.blade.php
@@ -1,27 +1,31 @@
-@props(['ends_at'])
+@props(['time', 'type' => 'ends'])
 
-@if (Carbon\Carbon::parse($ends_at) > now()->addMinutes(60))
-    <div>
-        ends {{ Carbon\Carbon::parse($ends_at)->diffForHumans() }}
+@php
+    $gerund = $type === 'ends' ? 'ending' : 'starting';
+@endphp
+
+@if (Carbon\Carbon::parse($time) > now()->addMinutes(60))
+    <div class="flex items-baseline space-x-1">
+        <span>{{ $type }}</span> <span>{{ Carbon\Carbon::parse($time)->diffForHumans() }}</span>
     </div>
 @else
     <div
         wire:ignore
         x-data="{
-            endTime: new Date('{{ $ends_at }}'),
+            endTime: new Date('{{ $time }}'),
             now: new Date(),
             timeLeft: '',
             interval: null,
             tick() {
                 let seconds = Math.floor((this.endTime - new Date()) / 1000);
                 if (seconds <= 0) {
-                    this.timeLeft = 'ending...';
+                    this.timeLeft = '{{ $gerund }}...';
                     return;
                 }
 
                 const m = Math.floor(seconds / 60).toString().padStart(2, '0');
                 const s = (seconds % 60).toString().padStart(2, '0');
-                this.timeLeft = `ends in ${m}:${s}`;
+                this.timeLeft = `{{ $type }} in ${m}:${s}`;
             }
         }"
         x-init="
