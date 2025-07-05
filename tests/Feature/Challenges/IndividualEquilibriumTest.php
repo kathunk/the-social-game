aeb10d895ac33fe5b89bc197a3d5d2f03fbae90a
John Drexler <johnrudolphdrexler@gmail.com>
2025-07-05 08:06:22 -0400

    WIP

---
+++ tests/Feature/Challenges/IndividualEquilibriumTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Challenges\Classes\IndividualEquilibrium;
+use App\Livewire\GameDashboard;
+use App\Models\Challenge;
+use Livewire\Livewire;
+use Thunk\Verbs\Facades\Verbs;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+it('runs the equilibrium challenge', function () {
+    Verbs::commitImmediately();
+
+    $challenges = [
+        [
+            'challenge_keys' => [IndividualEquilibrium::key()],
+            'duration' => 10,
+        ],
+    ];
+
+    $this->mockGameTemplate(
+        challenges: $challenges,
+        type: 'individual',
+    );
+
+    $this->createGame();
+
+    $player_2 = $this->createPlayer();
+    $player_3 = $this->createPlayer();
+    $player_4 = $this->createPlayer();
+
+    $this->game->start();
+
+    // the average is 10
+    incrementScore(30, player: $player_2);
+    incrementScore(10, player: $player_3);
+    incrementScore(0, player: $player_4);
+
+    // and the average is not affected by hidden points
+    incrementScore(100, player: $player_4, is_hidden: true);
+
+    $challenge = Challenge::first();
+
+    $this->actingAs($player_3->user);
+
+    Livewire::test(GameDashboard::class, ['game' => $this->game->fresh()])
+        ->assertSee('The average score (not including hidde points) is 10.')
+        ->set('round_properties.'.$challenge->class_key.'.upvote_player_id', $player_2->id)
+        ->set('round_properties.'.$challenge->class_key.'.downvote_player_id', $player_4->id)
+        ->call('callClassAction', 'vote', 'challenge', $challenge->class_key)
+        ->assertHasErrors()
+        ->set('round_properties.'.$challenge->class_key.'.upvote_player_id', $player_4->id)
+        ->set('round_properties.'.$challenge->class_key.'.downvote_player_id', $player_4->id)
+        ->call('callClassAction', 'vote', 'challenge', $challenge->class_key)
+        ->assertHasErrors()
+        ->set('round_properties.'.$challenge->class_key.'.upvote_player_id', $player_4->id)
+        ->set('round_properties.'.$challenge->class_key.'.downvote_player_id', $player_2->id)
+        ->call('callClassAction', 'vote', 'challenge', $challenge->class_key)
+        ->assertHasNoErrors();
+});
