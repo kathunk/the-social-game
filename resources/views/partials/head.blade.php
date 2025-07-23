757cf4f3659cd27eb84213e67f8eac52a64b4233
John Drexler <johnrudolphdrexler@gmail.com>
2025-07-23 17:48:32 -0400

    WIP

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
 <title>{{ $title ?? config('app.name') }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.bunny.net/css?family=fredoka:400,500,600" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
