3967806a8909ac31900b5092a0ee1bbb6a45f5f7
John Drexler <johnrudolphdrexler@gmail.com>
2025-07-25 07:13:35 -0400

    WIP

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,4 @@
 <div class="space-y-4">
-    <x-card class="flex flex-col items-center justify-center">
-        <flux:button variant="primary" :href="route('create-game')" class="mt-4">New Game</flux:button>
-    </x-card>
     @if ($this->games->filter(fn ($game) => $game->status === 'active')->count() > 0)
         <x-menus.game-card :games="$this->games->filter(fn ($game) => $game->status === 'active')" status="active" />
     @endif
@@ -9,6 +6,8 @@
     @if ($this->games->filter(fn ($game) => $game->status === 'upcoming')->count() > 0)
         <x-menus.game-card :games="$this->games->filter(fn ($game) => $game->status === 'upcoming')" status="upcoming" />
     @endif
+
+    <flux:button variant="primary" :href="route('create-game')" icon="plus" class="w-full">New Game</flux:button>
 
     @if ($this->games->filter(fn ($game) => $game->status === 'ended')->count() > 0)
         <x-menus.game-card :games="$this->games->filter(fn ($game) => $game->status === 'ended')" status="ended" />
