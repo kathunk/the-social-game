7e6c9d04f16fa7a213146f63f8fd28ac0e4f394d
Jacob Davis <jacobamdavis@gmail.com>
2025-07-28 15:44:34 -0500

    fixes

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,8 +1,31 @@
 @props(['attributes' => ''])
 
-<flux:button
-    class="!font-bold !px-4 !py-2 !h-auto !rounded-lg border-0" {{ $attributes }}
-    wire:loading.attr="disabled"
->
-    {{ $slot }}
-</flux:button>
+{{--
+    flux does not allow for conditional attributes inside tags,
+    so we disable wire:loading for alpine buttons by switching between two buttons
+--}}
+
+@if ($attributes && $attributes->has('@click'))
+    <flux:button
+        @class([
+            '!font-bold !px-4 !py-2 !h-auto !rounded-lg !shadow-none',
+            'border-0' => $attributes->has('variant'),
+            'border-1' => ! $attributes->has('variant')
+        ])
+        {{ $attributes }}
+    >
+        {{ $slot }}
+    </flux:button>
+@else
+    <flux:button
+        @class([
+            '!font-bold !px-4 !py-2 !h-auto !rounded-lg !shadow-none',
+            'border-0' => $attributes->has('variant'),
+            'border-1' => ! $attributes->has('variant')
+        ])
+        {{ $attributes }}
+        wire:loading.attr="disabled"
+    >
+        {{ $slot }}
+    </flux:button>
+@endif
