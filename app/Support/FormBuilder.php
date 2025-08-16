e7f7f346fcaa8409082135f20976e7af61252947
John Drexler <johnrudolphdrexler@gmail.com>
2025-08-16 12:49:20 -0400

    WIP

--- app/Support/FormBuilder.php
+++ app/Support/FormBuilder.php
@@ -6,12 +6,12 @@ use App\Challenges\Classes\BaseChallengeClass;
 use App\Challenges\Support\Interfaces\SupportsPeckingOrderBallots;
 use App\Challenges\Support\Interfaces\SupportsTeamSwaps;
 use App\Modifiers\Classes\BaseModifierClass;
-use App\Support\FormBuilderTraits\TierListFOrmElements;
+use App\Support\FormBuilderTraits\TierListFormElements;
 use Illuminate\Support\Collection;
 
 class FormBuilder
 {
-    use TierListFOrmElements;
+    use TierListFormElements;
 
     protected array $elements = [];
 
