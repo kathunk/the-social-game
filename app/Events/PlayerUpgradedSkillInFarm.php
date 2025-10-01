7c1bcea130112042834d2f064246ee65e685fa28
John Drexler <johnrudolphdrexler@gmail.com>
2025-09-30 21:15:01 -0400

    WIP

---
+++ app/Events/PlayerUpgradedSkillInFarm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Events;
+
+use Thunk\Verbs\Event;
+
+class PlayerUpgradedSkillInFarm extends Event
+{
+    public function handle()
+    {
+        // If I ain't have the keys to success I would've picked the lock. - Lil Wayne
+    }
+}
