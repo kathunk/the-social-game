40427556228314cf7cbd0bad33e0a3518dbe39ad
John Drexler <johnrudolphdrexler@gmail.com>
2025-11-08 08:24:23 -0500

    WIP

--- app/Events/PlayerJoinedFarmTeam.php
+++ app/Events/PlayerJoinedFarmTeam.php
@@ -80,6 +80,8 @@ class PlayerJoinedFarmTeam extends Event
             ->filter(fn ($space) => $space['stash_status']['player_owner_id'] === $this->player_id)
             ->sum(fn ($space) => $space['stash_status']['amount']);
 
+        $amount_to_transfer += $grain_in_stashes;
+
         if ($this->requester_previous_team_id !== null) {
             $previous_team = TeamState::load($this->requester_previous_team_id);
             $previous_team->player_ids = $previous_team->player_ids->reject(fn ($player_id) => $player_id === $this->player_id);
