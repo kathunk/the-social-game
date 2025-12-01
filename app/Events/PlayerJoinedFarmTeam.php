202fe60d8945ac61abf07edbc1a2e76e1e152f92
John Drexler <johnrudolphdrexler@gmail.com>
2025-12-01 10:15:54 -0500

    WIP

--- app/Events/PlayerJoinedFarmTeam.php
+++ app/Events/PlayerJoinedFarmTeam.php
@@ -74,17 +74,15 @@ class PlayerJoinedFarmTeam extends Event
         $team->player_ids->push($this->player_id);
         $requester = PlayerState::load($this->player_id);
 
-        $grain_in_stashes = collect($this->game()->modifiers()->firstWhere('class_key', FarmMap::key())->modifier_data)
-            ->filter(fn ($space) => $space['stash_status']['player_owner_id'] === $this->player_id)
-            ->sum(fn ($space) => $space['stash_status']['amount']);
-
         if ($this->requester_previous_team_id !== null) {
             $previous_team = TeamState::load($this->requester_previous_team_id);
             $previous_team->player_ids = $previous_team->player_ids->reject(fn ($player_id) => $player_id === $this->player_id);
         }
 
+        $amount_to_transfer = 0;
+
         if ($this->player_is_last_on_team) {
-            $amount_to_transfer = $previous_team->score();
+            $amount_to_transfer += $previous_team->score();
         }
 
         if ($this->requester_previous_team_id !== null) {
